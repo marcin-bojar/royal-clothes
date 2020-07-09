b7e3c9d50ceb9251297268f6f9a1b761d6668887
marcin-bojar <marcin.bojar@interia.pl>
2020-07-09 11:52:56 +0200

    added navigation functionality to MenuItem component using withRouter() HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+    <div 
+        className={`menu-item ${size}`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div style= { {backgroundImage: `url(${imageUrl})`} } className='background-image'/>
         <div className='content'>
             <h1 className='title'>{ title }</h1>
@@ -12,4 +16,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
