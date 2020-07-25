3e4a6b9320716b06ecaebff5ed478e532dd69450
marcin-bojar <marcin.bojar@interia.pl>
2020-07-25 15:06:47 +0200

    CollectionItem hover styles (Add to cart button and opacity)

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? 'google' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button className={`${isGoogleSignIn ? 'google' : ''} ${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
