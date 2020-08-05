cf03d326ad3684c35d125e5f3775fe627e2dd85b
marcin-bojar <marcin.bojar@interia.pl>
2020-08-05 13:28:18 +0200

    Added nested routes in shoPage component, collectionPage component and selectCollection shop selector

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
 import './shop.styles.scss';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={match.path} component={CollectionsOverview} />
+    <Route
+      exact
+      path={`${match.path}/:collectionId`}
+      component={CollectionPage}
+    />
   </div>
 );
 
